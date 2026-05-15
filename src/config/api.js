a18a1548b7ca36c51a192776ef26626c09c525b4
Hapema <haroldmartes62@gmail.com>
2026-05-15 14:41:08 -0400

    URL Code no hardcadiadas y en el api.js

---
+++ src/config/api.js
@@ -0,0 +1,3 @@
+const API_DESPACHOS = import.meta.env.VITE_API_DESPACHOS || 'http://localhost:8081';
+const API_VENTAS = import.meta.env.VITE_API_VENTAS || 'http://localhost:8082';
+export { API_DESPACHOS, API_VENTAS };
